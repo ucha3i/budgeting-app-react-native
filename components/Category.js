62367bd3294a337225e7c36795823ad13fbdac75
ucha3i <jchabik@gmail.com>
2020-03-12 17:42:53 +0100

    categories list

--- components/Category.js
+++ components/Category.js
@@ -6,7 +6,34 @@ export const Category = (props) => {
 
   return (
 
-    <Text>kategoria</Text>
+    <View style={styles.item}>
+      <Text
+        style={{
+          color: '#48506B',
+          margin: 7,
+          fontSize: 17,
+        }}
+      >
+        kategoria
+      </Text>
+    </View>
+
 
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
+});
--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import expenses from './expenses'
+import categories from './categories'
 
 export default combineReducers({
-  expenses
+  expenses,
+  categories
 })
